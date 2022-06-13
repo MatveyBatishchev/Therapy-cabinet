c67451b49bc82811ff7cf1a876a29cef6d67d958
dimevision <Fommkaa2304@mail.ru>
2022-06-13 10:51:52 +0300

    Добавлено исключение

---
+++ src/main/java/ru/mospolytech/therapy_cabinet/exception/RoleNotFoundException.java
@@ -0,0 +1,27 @@
+package ru.mospolytech.therapy_cabinet.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Dimevision
+ * @version 0.1
+ */
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RoleNotFoundException extends IllegalArgumentException {
+
+    public static final String ROLE_NOT_EXISTS = "Role %s does not exists in database";
+
+    public RoleNotFoundException(String message) {
+        super(message);
+    }
+
+    public RoleNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RoleNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
