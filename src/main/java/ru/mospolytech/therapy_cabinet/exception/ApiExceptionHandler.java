c9886e25a7919e53b21b7ce7dd4b1fd82ba045f2
dimevision <Fommkaa2304@mail.ru>
2022-06-22 15:08:17 +0300

    Добавил обработку исключений, связанных с некорректной отправкой запросов

---
+++ src/main/java/ru/mospolytech/therapy_cabinet/exception/ApiExceptionHandler.java
@@ -0,0 +1,62 @@
+package ru.mospolytech.therapy_cabinet.exception;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.*;
+
+/**
+ * @author Dimevision
+ * @version 0.1
+ * <p>
+ * Обработчик ошибок, связанных с некорректной отправкой запросов
+ */
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    @NotNull
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(@NotNull HttpMessageNotReadableException ex, @NotNull HttpHeaders headers, @NotNull HttpStatus status, @NotNull WebRequest request) {
+        String error = "Malformed JSON";
+        return buildResponseEntity(new ApiError(BAD_REQUEST, error, ex));
+    }
+
+    @Override
+    @NotNull
+    protected ResponseEntity<Object> handleExceptionInternal(@NotNull Exception ex, Object body, @NotNull HttpHeaders headers, @NotNull HttpStatus status, @NotNull WebRequest request) {
+        String error = "Illegal argument";
+        return buildResponseEntity(new ApiError(INTERNAL_SERVER_ERROR, error, ex));
+    }
+
+    @ExceptionHandler({BadCredentialsException.class})
+    protected ResponseEntity<Object> handleBadCredentials(BadCredentialsException ex) {
+        ApiError errorHandler = new ApiError(UNAUTHORIZED);
+        errorHandler.setMessage(ex.getMessage());
+
+        return buildResponseEntity(errorHandler);
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    protected ResponseEntity<Object> handleEntityNotFound(IllegalArgumentException ex) {
+        ApiError errorHandler = new ApiError(INTERNAL_SERVER_ERROR, ex);
+        errorHandler.setMessage(ex.getMessage());
+
+        return buildResponseEntity(errorHandler);
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
