528d602247409f58f1705a438dee24e354f9a4f7
dimevision <Fommkaa2304@mail.ru>
2022-09-12 21:27:29 +0300

    Добавил скан репозиториев для Redis и поменял часовой пояс на UTC:+03:00

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,15 +3,18 @@ package application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+import java.time.ZoneId;
 import java.util.TimeZone;
 
 @SpringBootApplication(scanBasePackages = {"ru.mospolytech.*", "common.*"})
 @MapperScan(basePackages = {"ru.mospolytech.therapy_cabinet.mybatis.*", "common.mybatis.*"})
+@EnableRedisRepositories(basePackages = "ru.mospolytech.*")
 public class Application {
 
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.systemDefault()));
         SpringApplication.run(Application.class, args);
     }
 
